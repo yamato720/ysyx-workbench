01d379e3ccff2f96dafaef81efd758a5a24bbfaf
yamato720 <pyx1128@outlook.com>
2026-04-07 13:31:19 +0800

    sync

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,12 +3,44 @@
 #include <memory.h>
 #include "syscall.h"
 
+#ifdef STRACE
+static const char *syscall_names[] = {
+  [SYS_exit]        = "exit",
+  [SYS_yield]       = "yield",
+  [SYS_open]        = "open",
+  [SYS_read]        = "read",
+  [SYS_write]       = "write",
+  [SYS_kill]        = "kill",
+  [SYS_getpid]      = "getpid",
+  [SYS_close]       = "close",
+  [SYS_lseek]       = "lseek",
+  [SYS_brk]         = "brk",
+  [SYS_fstat]       = "fstat",
+  [SYS_time]        = "time",
+  [SYS_signal]      = "signal",
+  [SYS_execve]      = "execve",
+  [SYS_fork]        = "fork",
+  [SYS_link]        = "link",
+  [SYS_unlink]      = "unlink",
+  [SYS_wait]        = "wait",
+  [SYS_times]       = "times",
+  [SYS_gettimeofday]= "gettimeofday",
+};
+#define NR_SYSCALLS (sizeof(syscall_names) / sizeof(syscall_names[0]))
+#endif
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1; // syscall number (a7)
   a[1] = c->GPR2; // arg0 (a0)
   a[2] = c->GPR3; // arg1 (a1)
   a[3] = c->GPR4; // arg2 (a2)
+
+#ifdef STRACE
+  const char *name = (a[0] < NR_SYSCALLS && syscall_names[a[0]])
+                     ? syscall_names[a[0]] : "unknown";
+  Log("[strace] %s(%lu, %lu, %lu)", name, a[1], a[2], a[3]);
+#endif
 
   switch (a[0]) {
     case SYS_exit:
@@ -75,4 +107,8 @@ void do_syscall(Context *c) {
 
     default: panic("Unhandled syscall ID = %lu", a[0]);
   }
+
+#ifdef STRACE
+  Log("[strace]   => %ld", (intptr_t)c->GPRx);
+#endif
 }
